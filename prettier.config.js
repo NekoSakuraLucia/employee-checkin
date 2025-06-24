8797dc32200308aaecee8d925fead3292defd956
NekoSakuraLucia <mokurolucia@gmail.com>
2025-06-24 18:27:30 +0700

    install shdacn-ui & tailwind & prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+};
+
+export default config;
